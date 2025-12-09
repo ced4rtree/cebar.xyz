c869f4b87550176ecf836e9184bb075ad81ee422
ced4rtree <usernamesarenoteasy@proton.me>
2025-12-09 15:24:10 -0700

    add package output to nix flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    self.submodules = true; # tell nix to include the risotto theme submodule
+
   };
 
-  outputs = { nixpkgs, ... }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        hugo
-        go
-      ];
-    };
-  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
+      systems = import inputs.systems;
+      perSystem = { pkgs, system, self', ... }: {
+        packages.cebar_xyz = pkgs.stdenvNoCC.mkDerivation {
+          pname = "cebar.xyz";
+          version = "1.0";
+
+          src = ./.;
+
+          buildInputs = [ pkgs.hugo ];
+
+          buildPhase = ''
+            hugo build -d output
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r output/* $out/
+          '';
+        };
+
+        packages.default = self'.packages.cebar_xyz;
+
+        devShells.${system}.default = pkgs.mkShell {
+          packages = with pkgs; [
+            hugo
+            go
+          ];
+        };
+      };
+    });
 }
